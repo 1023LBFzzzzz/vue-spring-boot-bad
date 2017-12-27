3c756667cfb9e7beb77c8fe8ba4f5fdc9d5f986b
yuwei35kd <yuwei35kd@163.com>
2017-12-27 16:17:16 +0800

    vue sprint boot bad

---
+++ src/main/java/yuwei35kd/vue/boot/VueSpringBootBadApplication.java
@@ -0,0 +1,12 @@
+package yuwei35kd.vue.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueSpringBootBadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueSpringBootBadApplication.class, args);
+	}
+}
